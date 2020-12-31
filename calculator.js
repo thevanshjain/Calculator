ae033fec52ef8d86748b79ff618c8fe3e0c96678
vanshjain2810 <vanshvasu2810@gmail.com>
2020-12-31 19:37:53 +0530

    Vansh's Calculator v1.0

---
+++ calculator.js
@@ -0,0 +1,75 @@
+let result=document.querySelector('.head');
+let tap=document.querySelector('.calculation');
+let prev='';
+let temp='';
+let operator='';
+
+function simplify(symbol) {
+    
+    if(temp=='')
+        return;
+    
+    if(prev==='') {
+        prev=parseInt(temp);
+        operator=symbol;
+    }
+    else {
+    switch(operator)  {
+        case '+': prev+=parseInt(temp);
+                  operator='';
+                break;
+        case '-': prev-=parseInt(temp);
+                  operator='';
+                break;
+        case 'x': prev*=parseInt(temp);
+                  operator='';
+                break;
+        case '÷': prev/=parseInt(temp);
+                  operator='';
+                break;
+    }
+    }
+    operator=symbol;
+    result.innerText=parseInt(prev);
+    temp='';
+}
+function tapped(symbol) {
+    
+    if(parseInt(symbol)>=0 && parseInt(symbol)<10) {
+        temp+=symbol;
+        result.innerText=temp;
+    }
+    else if(symbol==='C') {
+        prev='';
+        temp='';
+        next='';
+        operator='';
+        result.innerText=0;
+    }
+    else if(symbol==='←') {
+        temp=temp.substring(0,temp.length-1);
+        if(temp.length==0) {
+            temp='';
+            result.innerText=0;
+        }
+        else {
+        result.innerText=temp;
+        }
+    }
+    else if(symbol==='=') {
+        if(operator!=='')
+            simplify(operator);
+        temp=prev;
+        prev='';
+        // operator='';
+    //    result.innerText=prev; 
+    }
+    else {
+        simplify(symbol);
+    }
+}
+function clicked() {
+    
+    tap.addEventListener('click', (event) => tapped(event.target.innerText) );
+}
+clicked();
